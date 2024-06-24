8016b765e30112cbca1cde6e7371c4dd21e29eb0
Josh <1029946@student.sae.edu.au>
2024-06-24 17:22:22 +1000

    yupppp

--- Assets/Scripts/RopeManagerJosh.cs
+++ Assets/Scripts/RopeManagerJosh.cs
@@ -1,39 +1,31 @@
-    using System.Collections;
+using System;
+using System.Collections;
 using System.Collections.Generic;
-using System.Drawing;
-using System.Numerics;
-using System.Security.Cryptography;
-using System.Text.RegularExpressions;
-using Unity.Burst.Intrinsics;
-using Unity.VisualScripting;
-using UnityEditor.Experimental.GraphView;
 using UnityEngine;
-using UnityEngine.PlayerLoop;
-using UnityEngine.UIElements;
-using UnityEngine.XR;
+
 
 public class RopeManagerJosh : MonoBehaviour
 {
    
     
 
-    // Node is a class like : (add public and stuff, i'm lazy)
-    //public Vector2 position;
-    //public Vector2 previousPosition;
+    // Node is a class like : (add public and stuff, I'm lazy)
+    public Vector2 position;
+    public Vector2 previousPosition;
     private List<Node> nodes = new List<Node>();
-    //public List<Constraint> constraints = new List<Constraint>();
+    private List<Constraint> constraints = new List<Constraint>();
     class Node
     {
-        
+        this.n = new RopeState(position);
         public float mass; // if you wanted to have mass
         public bool isFixed; // bool fixed; // if true, don't do integration, it's position is locked.
     }
 
     public void Integrate()
     {
-        //Vector2 newPosition = position + (position - previousPosition);
-        //previousPosition = position;
-        //position = newPosition; 
+        Vector2 newPosition = position + (position - previousPosition);
+        previousPosition = position;
+        position = newPosition; 
     }
 
     class Constraint
@@ -47,14 +39,12 @@ public class RopeManagerJosh : MonoBehaviour
 
     
 
-    //public int AddNode(Vector2 position, float mass, bool isFixed)
-    //{
-        // The addNode returns the index number of the node it added(so 0, then 1, then 2, etc).So you could make a square like this:
-        // int corner1 = addNode(new Vector2(0, 0), 1, false);
-        // int corner2 = addNode(new Vector2(1, 0), 1, false);
-        // int corner3 = addNode(new Vector2(1, 1), 1, false);
-        // int corner4 = addNode(new Vector2(0, 1), 1, false);
-    //}
+    public int AddNode(Vector2 position, float mass, bool isFixed)
+    {
+        Node newNode = new Node(position, mass, isFixed);
+        nodes.Add(newNode);
+        return nodes.Count - 1;
+    }
 
     public void AddConstraint(int node1, int node2, float desiredDistance, float compensate1 = 0.5f, float compensate2 = 0.5f)
     {
@@ -69,26 +59,26 @@ public class RopeManagerJosh : MonoBehaviour
    
 
     // Also handy would be an addConstraint that doesn't take a distance, it calculates it manually by getting the distance between the two nodes (makes diagonals easier).
-    private void FixedUpdate()
+    private void FixedUpdate() 
     {
     
-	// Loop over nodes
+	 // Loop over nodes
 
-    // Add gravity to each(with addForce)
+     // Add gravity to each(with addForce)
 
-    // Integrate each node
+     // Integrate each node
 
-    // Loop 10 times(or more / less)
+     // Loop 10 times(or more / less)
 
-    // Loop over constraints
+     // Loop over constraints
 
-    // Do fixed distance constraint between the constraint's node1 and node2
+     // Do fixed distance constraint between the constraint's node1 and node2
 
-    // Like: constraintfixeddist(ref nodeList[constraint.node1].state.pos, ref nodeList[constraint.node2].state.pos, constraint.desiredDistance, constraint.compensate1, constraint.compensate2);
-    // Loop over nodes
-    // Apply ground check, move nodes to ground if they go below.
-    // Loop over nodes
-    // Copy node positions into game objects if you have visuals.
+     // Like: constraintfixeddist(ref nodeList[constraint.node1].state.pos, ref nodeList[constraint.node2].state.pos, constraint.desiredDistance, constraint.compensate1, constraint.compensate2);
+     // Loop over nodes
+     // Apply ground check, move nodes to ground if they go below.
+     // Loop over nodes
+     // Copy node positions into game objects if you have visuals.
     }
     
 
